b335fa38171d7dbd4bd02eee7e6d6ca70cf81962
xiaoyu zhou <xiaoyuz2011@gmail.com>
2018-01-26 01:12:05 -0500

    init

---
+++ R/init.R
@@ -0,0 +1,17 @@
+#' print hello world by default
+#'
+#' @param boolean greeting
+#' @return void
+#' @seealso \code{\link{nchar}} which this function wraps
+#' @export
+#' @examples
+#' init()
+#' init(FALSE)
+init <- function(greeting=TRUE){
+  if(greeting){
+    print("Hello World")
+  }else{
+    print("Slience")
+  }
+  return
+}
